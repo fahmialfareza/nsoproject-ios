e700b14c81f2af3f0ce6d532bd2ab79ab7cf6883
Fahmi Alfareza <fahmialfareza@icloud.com>
2019-10-25 20:30:51 +0700

    Initial Commit

---
+++ NSO Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NSO Project
+//
+//  Created by Fahmi Alfareza on 25/10/19.
+//  Copyright © 2019 CV. Karya Studio Teknologi Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
